99194fe9febb8e0ebb4d13f0749f7d49ba928e12
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-23 08:25:12 +0200

    add artist model + update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import {MatMenuModule} from '@angular/material/menu';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu/menu.component';
+import { ArtistDetailsComponent } from './artist/artist-details/artist-details.component';
+import { ArtistListComponent } from './artist/artist-list/artist-list.component';
+import { ArtistCardComponent } from './artist/artist-card/artist-card.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ArtistDetailsComponent,
+    ArtistListComponent,
+    ArtistCardComponent
   ],
   imports: [
     BrowserModule,
