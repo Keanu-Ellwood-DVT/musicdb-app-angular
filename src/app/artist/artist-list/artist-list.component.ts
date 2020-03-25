de0658bb299aa31919fb8bc4fbaf0c6b4d89df4f
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-25 15:16:00 +0200

    update artist-list

--- src/app/artist/artist-list/artist-list.component.ts
+++ src/app/artist/artist-list/artist-list.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  SimpleChanges,
+  OnChanges
+} from "@angular/core";
+import { Artists } from "../../models/artists";
+import { Artist } from "../../models/artist";
+import { ArtistsService } from "../../services/artists.service";
+import { of } from "rxjs";
+import { debounceTime, tap, map, switchAll } from "rxjs/operators";
+import { MenuComponent } from "src/app/menu/menu.component";
+import { QueryService } from "src/app/services/query.service";
 
 @Component({
-  selector: 'app-artist-list',
-  templateUrl: './artist-list.component.html',
-  styleUrls: ['./artist-list.component.css']
+  selector: "app-artist-list",
+  templateUrl: "./artist-list.component.html",
+  styleUrls: ["./artist-list.component.css"]
 })
 export class ArtistListComponent implements OnInit {
+  @Input() query: string;
 
-  constructor() { }
+  artists: Artists;
+
+  constructor(
+    private artistsService: ArtistsService,
+    private queryService: QueryService
+  ) {}
 
   ngOnInit(): void {
-  }
+    // this.artistsService.getInitialArtists().subscribe(x => {
+    //   this.artists = x;
+    // });
 
+    this.artistsService
+          .getTopArtists()
+          .subscribe(x => {
+            //this.artists = new Artists();
+            this.artists.data = x;
+            this.artists.next = "";
+          });
+
+    this.queryService.queryString$.subscribe(query => {
+      if (query === "") {
+        this.artistsService
+        .getInitialArtists()
+        .subscribe(x => {
+          debounceTime(500),
+          this.artists = x;
+        });
+
+        // this.artistsService
+        //   .getTopArtists()
+        //   .subscribe(x => {
+        //     this.artists.data = x;
+        //     this.artists.next = "";
+        //   });
+
+      }
+      else {
+        this.artistsService
+          .getArtists(query)
+          .subscribe(x => {
+            debounceTime(500),
+            this.artists = x
+          });
+      }
+    });
+  }
 }
