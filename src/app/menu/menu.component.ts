aa859c58828f28137d92042b0b4e20b6c1af6bbd
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-25 15:15:43 +0200

    update menu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { QueryService } from "../services/query.service";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { debounceTime } from 'rxjs/operators';
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.css"]
 })
 export class MenuComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private queryService: QueryService, private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  get query() {
+    return this.form.get("query");
   }
 
+  ngOnInit() {
+    this.form = this.fb.group({
+      query: null
+    });
+
+    this.query.valueChanges.subscribe(value => {
+    	this.queryService.updateQuery(value);
+    });
+  }
 }
