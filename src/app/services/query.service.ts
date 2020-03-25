6a35cf92293007f6d06e3ff4d77f19b762d8f5cd
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-25 15:16:48 +0200

    create query service

---
+++ src/app/services/query.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { Observable, BehaviorSubject } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class QueryService {
+
+  query$: BehaviorSubject<string>;
+
+  constructor() {
+    this.query$ = new BehaviorSubject("");
+  }
+
+  get queryString$(): Observable<string> {
+    return this.query$.asObservable();
+  }
+
+  updateQuery(query: string) {
+    this.query$.next(query);
+  }
+}
