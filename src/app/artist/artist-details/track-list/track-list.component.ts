d8c5bb43a40a0d62043d8323ee2cd6c859948af8
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-25 15:15:27 +0200

    update track list

---
+++ src/app/artist/artist-details/track-list/track-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Track } from "src/app/models/track";
+import {MatDividerModule} from '@angular/material/divider';
+
+@Component({
+  selector: "app-track-list",
+  templateUrl: "./track-list.component.html",
+  styleUrls: ["./track-list.component.css"]
+})
+export class TrackListComponent implements OnInit {
+  @Input()
+  track: Track;
+
+  trackLength: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.convertTrackTime();
+  }
+
+  convertTrackTime() {
+    var value = this.track.duration;
+    this.trackLength =
+      Math.floor(value / 60) + ":" + (value % 60 ? value % 60 : "00");
+  }
+}
