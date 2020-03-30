2be10aa7c397ff29cf335baa14d5f6b9a81155b5
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-30 08:43:42 +0200

    Remove comments, unused imports, if check

--- src/app/services/artists.service.ts
+++ src/app/services/artists.service.ts
@@ -4,7 +4,6 @@ import { Observable, forkJoin } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Artists } from "../models/artists";
 import { Artist } from "../models/artist";
-import { mergeAll, toArray } from 'rxjs/operators';
 
 @Injectable({
   providedIn: "root"
@@ -13,7 +12,6 @@ export class ArtistsService {
   constructor(private http: HttpClient) {}
 
   getInitialArtists(): Observable<Artists> {
-    //Dummy query
     return this.http.get<Artists>(`${environment.apiUri}/search/artist?q=deer`);
   }
 
@@ -21,30 +19,18 @@ getTopArtists(): Observable<Artist[]> {
     let artists$: Observable<Artist>[] = [];
 
     for (let i = 1; i < 22; i++) {
-      if (i === 12) {
-
-      }else{
         artists$.push(
           this.http.get<Artist>(
             `${environment.apiUri}/artist/${i}`
           )
         );
-      }
-
     }
-    forkJoin(artists$).subscribe(x => console.log(x))
     return forkJoin(artists$);
 }
-
-// getOneArtist(): Observable<Artist[]> {
-
-//   return this.http.get<Artist[]>( `${environment.apiUri}/artist/1`);
-// }
-
 
   getArtists(query?: string): Observable<Artists> {
     return this.http.get<Artists>(
       `${environment.apiUri}/search/artist${query ? `?q=${query}` : ""}`
-    ); //https://api.deezer.com/search/artist?q=deer?limit=50
+    );
   }
 }
