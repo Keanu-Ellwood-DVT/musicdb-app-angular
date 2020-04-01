1c4623a64ccce1646f5603562f54eefa79495ffa
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-04-01 09:56:44 +0200

    Fix eslint errors

--- src/app/artist/artist-details/artist-details.component.ts
+++ src/app/artist/artist-details/artist-details.component.ts
@@ -1,47 +1,43 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Artist } from '../../models/artist';
-import { ActivatedRoute} from '@angular/router';
-import { ArtistDetailsService } from 'src/app/services/artist-details.service';
-import { Tracks } from 'src/app/models/tracks';
-import { Albums } from 'src/app/models/albums';
-import { SearchStateService } from 'src/app/services/search-state.service';
+import { Component, OnInit, Input } from '@angular/core'
+import { Artist } from '../../models/artist'
+import { ActivatedRoute } from '@angular/router'
+import { ArtistDetailsService } from 'src/app/services/artist-details.service'
+import { Tracks } from 'src/app/models/tracks'
+import { Albums } from 'src/app/models/albums'
+import { SearchStateService } from 'src/app/services/search-state.service'
 @Component({
   selector: 'app-artist-details',
   templateUrl: './artist-details.component.html',
-  styleUrls: ['./artist-details.component.css']
+  styleUrls: ['./artist-details.component.css'],
 })
 export class ArtistDetailsComponent implements OnInit {
+  @Input() query: string
 
-  @Input() query: string;
+  artist: Artist
+  tracks: Tracks
+  albums: Albums
 
-  artist: Artist;
-  tracks: Tracks;
-  albums: Albums;
-  
   constructor(
     private route: ActivatedRoute,
     private artistDetailsService: ArtistDetailsService,
-    private searchStateService: SearchStateService
-
+    private searchStateService: SearchStateService,
   ) {}
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id')
 
-    const id = this.route.snapshot.paramMap.get('id');
-
-    this.searchStateService.setMessage(false);
+    this.searchStateService.setMessage(false)
 
     this.artistDetailsService.getArtistDetails(id).subscribe(x => {
-      this.artist = x;
-    });
+      this.artist = x
+    })
 
     this.artistDetailsService.getArtistTopTracks(id).subscribe(x => {
-      this.tracks = x;
-    });
+      this.tracks = x
+    })
 
     this.artistDetailsService.getArtistAlbums(id).subscribe(x => {
-      this.albums = x;
-    });
+      this.albums = x
+    })
   }
-
 }
